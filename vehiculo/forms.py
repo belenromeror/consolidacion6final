7039d97a93400fbc9296da87b9773ebf527ade47
Tu nombre de usuario en GitHub <Tu direccin de correo electrnico en GitHub>
2023-07-20 23:38:47 -0400

    ultimos cambios

--- vehiculo/forms.py
+++ vehiculo/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import Vehiculo
 from .models import Usuario
+from django.shortcuts import render
+
 class VehiculoForm(forms.ModelForm):
     class Meta:
         model = Vehiculo
@@ -11,3 +13,15 @@ class RegistroUsuarioForm(UserCreationForm):
     class Meta:
         model = Usuario
         fields = ('username', 'password1')
+
+
+def registro_usuario(request):
+    if request.method == 'POST':
+        form = RegistroUsuarioForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')  # Redirigir a la página de inicio después del registro exitoso
+    else:
+        form = RegistroUsuarioForm()
+
+    return render(request, 'registro_usuario.html', {'form': form})
