ae1ae210f4573dc3eec4bdfa2c14122652aaba13
Tu nombre de usuario en GitHub <Tu direccin de correo electrnico en GitHub>
2023-07-20 23:26:51 -0400

    solución de errores

--- vehiculo/apps.py
+++ vehiculo/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class VehiculoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'vehiculo'
@@ -10,5 +9,6 @@ class VehiculoConfig(AppConfig):
         from django.db.models.signals import post_save
 
         # Conectar las señales a los receptores
+        from .models import asignar_permiso_visualizar_catalogo, asignar_permiso_can_add_vehiculo
         post_save.connect(asignar_permiso_visualizar_catalogo, sender=Usuario)
         post_save.connect(asignar_permiso_can_add_vehiculo, sender=Usuario)
--- vehiculo/models.py
+++ vehiculo/models.py
@@ -7,6 +7,9 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from .signals import asignar_permiso_visualizar_catalogo
+from django.utils.translation import gettext_lazy as _
+
+
 class Vehiculo(models.Model):
     MARCA_CHOICES = [
         ('Fiat', 'Fiat'),
@@ -33,9 +36,33 @@ class Vehiculo(models.Model):
     def __str__(self):
         return f"{self.marca} - {self.modelo}"
 
+
 class Usuario(AbstractUser):
     nombre = models.CharField(max_length=15, blank=True, null=True)
     contraseña = models.CharField(max_length=100, blank=True, null=True)
+
+    # Agregar related_name para evitar conflictos con User.groups
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name=_("groups"),
+        blank=True,
+        help_text=_(
+            "The groups this user belongs to. A user will get all permissions "
+            "granted to each of their groups."
+        ),
+        related_name="usuarios",  # Cambiar "usuarios" por un nombre único
+        related_query_name="usuario",
+    )
+
+    # Agregar related_name para evitar conflictos con User.user_permissions
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=_("user permissions"),
+        blank=True,
+        help_text=_("Specific permissions for this user."),
+        related_name="usuarios_permissions",  # Cambiar "usuarios_permissions" por un nombre único
+        related_query_name="usuario",
+    )
 
     def __str__(self):
         return self.username
@@ -50,9 +77,11 @@ def asignar_permiso_visualizar_catalogo(instance, created, **kwargs):
         )
         instance.user_permissions.add(permission)
 
+
 @receiver(post_save, sender=Usuario)
 def on_usuario_post_save(sender, instance, created, **kwargs):
     asignar_permiso_visualizar_catalogo(sender, instance, created, **kwargs)
+
 
 def asignar_permiso_can_add_vehiculo(instance, created, **kwargs):
     if created:
@@ -63,4 +92,3 @@ def asignar_permiso_can_add_vehiculo(instance, created, **kwargs):
             content_type=content_type
         )
         instance.user_permissions.add(permission)
-
